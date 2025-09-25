645b461fc2860d256e33b4ba8f5ede8e7f2cd0b3
Mohammed Bahy <mohammedbahy10122004@gmail.com>
2025-09-25 15:44:34 +0300

    MVVM App added

---
+++ app/src/main/java/com/bahy/mvvmapp/CounterViewModel.kt
@@ -0,0 +1,7 @@
+package com.bahy.mvvmapp
+
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel() {
+    var counter =0
+}
---
+++ app/src/main/java/com/bahy/mvvmapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.bahy.mvvmapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.bahy.mvvmapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val viewModel = ViewModelProvider(this)[CounterViewModel::class.java]
+        binding.counter.text="%,d".format(viewModel.counter)
+
+        binding.btn.setOnClickListener{
+            viewModel.counter++
+            binding.counter.text="%,d".format(viewModel.counter)
+        }
+    }
+}
